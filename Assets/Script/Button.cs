d8cb08fa9eedbb148e2b4df827f2e2295d56a1b8
AzraNA24 <azra.syarifuddin@gmail.com>
2024-12-01 21:26:34 +0700

    Nambah reset trigger

--- Assets/Script/Button.cs
+++ Assets/Script/Button.cs
@@ -1,21 +1,36 @@
 using UnityEngine;
 
-public class Button : MonoBehaviour
+public class ButtonActionHandler : MonoBehaviour
 {
     public Animator animator;
+
     public void OnDrinkPotionButtonPressed()
     {
         animator.SetTrigger("Drink");
+        StartCoroutine(ReturnToStanceAfterAnimation("Drink"));
     }
 
-    // Method for the "Long Range" button
     public void OnLongRangeButtonPressed()
     {
         animator.SetTrigger("LongRange");
+        StartCoroutine(ReturnToStanceAfterAnimation("LongRange"));
     }
-
     public void OnShortRangeButtonPressed()
     {
         animator.SetTrigger("ShortRange");
+        StartCoroutine(ReturnToStanceAfterAnimation("ShortRange"));
+    }
+
+    private System.Collections.IEnumerator ReturnToStanceAfterAnimation(string animationName)
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        while (!stateInfo.IsName(animationName))
+        {
+            stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            yield return null;
+        }
+        yield return new WaitForSeconds(stateInfo.length);
+
+        animator.ResetTrigger("Drink"); 
     }
 }
