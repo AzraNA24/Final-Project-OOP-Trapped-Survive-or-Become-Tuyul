4c7c4480301b42419ed83dafaa4ee670ab9a5fa8
AzraNA24 <azra.syarifuddin@gmail.com>
2024-12-10 16:00:59 +0700

    Paket

--- Assets/Script/LootBox.cs
+++ Assets/Script/LootBox.cs
@@ -4,8 +4,8 @@ public class LootBox : MonoBehaviour
 {
     public void GenerateLoot()
     {
-        int money = Random.Range(1, 6);
-        int potion = Random.Range(0, 3);
+        int money = Random.Range(1, 4);
+        int potion = Random.Range(0, 2);
         int bullet = Random.Range(0, 3);
 
         Debug.Log($"Receive: Money = {money}, Potion = {potion}, Bullet = {bullet}");
