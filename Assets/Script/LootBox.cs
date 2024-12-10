f8d1cd45ef61898e5f2c6219588f9e6f4ea0d59c
Amel <sitiaamalianr29@gmail.com>
2024-12-10 17:03:22 +0700

    Room

--- Assets/Script/LootBox.cs
+++ Assets/Script/LootBox.cs
@@ -4,9 +4,9 @@ public class LootBox : MonoBehaviour
 {
     public void GenerateLoot()
     {
-        int money = Random.Range(5, 11);
-        int potion = Random.Range(0, 3);
-        int bullet = Random.Range(0, 4);
+        int money = Random.Range(1, 4);
+        int potion = Random.Range(0, 2);
+        int bullet = Random.Range(0, 3);
 
         Debug.Log($"Receive: Money = {money}, Potion = {potion}, Bullet = {bullet}");
 
